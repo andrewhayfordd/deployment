7511155dec0f388d239d37dc2baefef70d1f970b
andrewhayford <andrewhayford0@gmail.com>
2023-06-15 22:52:03 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
 import Footer from './Components/Footer/Footer';
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
-import Contact from './Pages/Contact/Contact';
-import { IndexRouteObject } from 'react-router-dom';
 
 function App() {
   return (
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
 import "./fontawesome-free-5.15.4-web/css/all.min.css";
 
